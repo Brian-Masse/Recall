983c52626e727d15ff18b8731a650a1f9ce1288e
Brian Masse <brianm25it@gmail.com>
2024-01-25 22:04:05 -0800

    adopted LargeRoundedButton

--- Recall/Universals/UniversalButtons.swift
+++ Recall/Universals/UniversalButtons.swift
@@ -153,61 +153,61 @@ struct LargeFormRoundedButton: View {
 }
 
 //MARK: LargeRoundedButton
-struct LargeRoundedButton: View {
-    
-    let label: String
-    let completedLabel: String
-    let icon: String
-    let completedIcon: String
-    
-    let completed: () -> Bool
-    let action: () -> Void
-    
-    let small: Bool 
-    let wide: Bool
-    let color: Color?
-    
-    @State var tempCompletion: Bool = false
-    
-    init( _ label: String, to completedLabel: String = "", icon: String, to completedIcon: String = "", wide: Bool = false, small: Bool = false, color: Color? = nil, completed: @escaping () -> Bool = {false}, action: @escaping () -> Void ) {
-        self.label = label
-        self.completedLabel = completedLabel
-        self.icon = icon
-        self.completedIcon = completedIcon
-        self.completed = completed
-        self.action = action
-        self.wide = wide
-        self.small = small
-        self.color = color
-    }
-    
-    var body: some View {
-        let label: String = (self.completed() || tempCompletion ) ? completedLabel : label
-        let completedIcon: String = (self.completed() || tempCompletion ) ? completedIcon : icon
-        
-        HStack {
-            if wide { Spacer() }
-            if label != "" {
-                UniversalText(label, size: Constants.UISubHeaderTextSize, font: Constants.titleFont)
-                    .minimumScaleFactor(0.7)
-                    .lineLimit(1)
-            }
-            
-            if completedIcon != "" {
-                Image(systemName: completedIcon)
-            }
-            if wide { Spacer() }
-        }
-        .padding(.vertical, small ? 7: 25 )
-        .padding(.horizontal, small ? 25 : 25)
-        .foregroundColor(.black)
-        .if( color == nil ) { view in view.universalStyledBackgrond(.accent) }
-        .if( color != nil ) { view in view.background(color) }
-        .cornerRadius(Constants.UIDefaultCornerRadius)
-        .animation(.default, value: completed() )
-        .onTapGesture { action() }
-    }
-}
+//struct LargeRoundedButton: View {
+//    
+//    let label: String
+//    let completedLabel: String
+//    let icon: String
+//    let completedIcon: String
+//    
+//    let completed: () -> Bool
+//    let action: () -> Void
+//    
+//    let small: Bool 
+//    let wide: Bool
+//    let color: Color?
+//    
+//    @State var tempCompletion: Bool = false
+//    
+//    init( _ label: String, to completedLabel: String = "", icon: String, to completedIcon: String = "", wide: Bool = false, small: Bool = false, color: Color? = nil, completed: @escaping () -> Bool = {false}, action: @escaping () -> Void ) {
+//        self.label = label
+//        self.completedLabel = completedLabel
+//        self.icon = icon
+//        self.completedIcon = completedIcon
+//        self.completed = completed
+//        self.action = action
+//        self.wide = wide
+//        self.small = small
+//        self.color = color
+//    }
+//    
+//    var body: some View {
+//        let label: String = (self.completed() || tempCompletion ) ? completedLabel : label
+//        let completedIcon: String = (self.completed() || tempCompletion ) ? completedIcon : icon
+//        
+//        HStack {
+//            if wide { Spacer() }
+//            if label != "" {
+//                UniversalText(label, size: Constants.UISubHeaderTextSize, font: Constants.titleFont)
+//                    .minimumScaleFactor(0.7)
+//                    .lineLimit(1)
+//            }
+//            
+//            if completedIcon != "" {
+//                Image(systemName: completedIcon)
+//            }
+//            if wide { Spacer() }
+//        }
+//        .padding(.vertical, small ? 7: 25 )
+//        .padding(.horizontal, small ? 25 : 25)
+//        .foregroundColor(.black)
+//        .if( color == nil ) { view in view.universalStyledBackgrond(.accent) }
+//        .if( color != nil ) { view in view.background(color) }
+//        .cornerRadius(Constants.UIDefaultCornerRadius)
+//        .animation(.default, value: completed() )
+//        .onTapGesture { action() }
+//    }
+//}
 
 
 //MARK: ConditionalLargeRoundedButton
