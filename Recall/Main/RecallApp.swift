45291c95a3c88ee700eaaea6e7346290f26a2547
Brian Masse <brianm25it@gmail.com>
2024-11-06 12:17:15 -0800

    decreased SubHeaderFontSize

--- Recall/Main/RecallApp.swift
+++ Recall/Main/RecallApp.swift
@@ -28,8 +28,8 @@ struct RecallApp: App {
         Constants.setFontSizes(UILargeTextSize: 90,
                                UITitleTextSize: 45,
                                UIMainHeaderTextSize: 35,
-                               UIHeaderTextSize: 30,
-                               UISubHeaderTextSize: 20,
+                               UIHeaderTextSize: 25,
+                               UISubHeaderTextSize: 16,
                                UIDefeaultTextSize: 15,
                                UISmallTextSize: 11)
         
