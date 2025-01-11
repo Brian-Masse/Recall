58fd4add4358724fd5cd6503aae87a628ddfe041
Brian Masse <brianm25it@gmail.com>
2025-01-10 23:24:41 -0800

    added life calendar

---
+++ Recall/Calendar/Views/LifeView.swift
@@ -0,0 +1,94 @@
+//
+//  LifeView.swift
+//  Recall
+//
+//  Created by Brian Masse on 1/10/25.
+//
+
+import Foundation
+import SwiftUI
+import UIUniversals
+
+struct LifeView: View {
+    @State private var referenceDate: Date = .now
+    
+    @State private var currentMonth: Int = 0
+    
+    private let rowCount: Double = 24
+    private let spacing: Double = 2
+    
+    private let expectedYears: Double = 90
+    @State private var numberOfCells: Int = 0
+    
+//    MARK: setup
+    private func setup() {
+        
+        let componets = DateComponents(year: 2005, month: 5, day: 18)
+        let date = Calendar.current.date(from: componets)!
+        self.referenceDate = date
+        
+        self.numberOfCells = Int(expectedYears * 12)
+        self.currentMonth = Int(Date.now.timeIntervalSince( referenceDate ) / (Constants.yearTime) * 12)
+    }
+    
+//    MARK: Header
+    @ViewBuilder
+    private func makeHeader() -> some View {
+        HStack {
+            VStack(alignment: .leading) {
+                UniversalText( "Life Progress", size: Constants.UIHeaderTextSize, font: Constants.titleFont )
+                
+                UniversalText( "\(currentMonth) / \(numberOfCells)", size: Constants.UIDefaultTextSize, font: Constants.mainFont )
+                    .opacity(0.75)
+            }
+            
+            Spacer()
+        }
+    }
+    
+//    MARK: makeCalendar
+    @ViewBuilder
+    private func makeCalendar() -> some View {
+        GeometryReader { geo in
+            
+            let width = (geo.size.width - (1 + rowCount) * spacing) / rowCount
+            
+            LazyVGrid(columns: [ .init(.adaptive(minimum: width, maximum: width),
+                                       spacing: spacing,
+                                       alignment: .center) ],
+                      alignment: .center,
+                      spacing: spacing) {
+                
+                ForEach( 0..<numberOfCells, id: \.self ) { i in
+                    Circle()
+                        .frame(width: width, height: width)
+                        .opacity( i > currentMonth ? 0.2 : 1 )
+
+                }
+            }
+        }
+    }
+    
+    
+    
+//    MARK: - body
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            makeHeader()
+            
+            makeCalendar()
+                .universalStyledBackgrond(.accent, onForeground: true)
+        }
+        .padding()
+        .task { setup() }
+        
+        
+    }
+    
+}
+
+
+#Preview {
+    LifeView()
+}
